67c1d623f399c7d5df939875cb98e72d95807838
RDonat <rasheeddonat@gmail.com>
2019-12-17 14:06:09 -0500

    added base js, linking, and music

--- script.js
+++ script.js
@@ -4,10 +4,46 @@
   
 */
 
+// Card Variables
 let suits = ['Spades', 'Diamonds', 'Hearts', 'Clubs'];
 let values = ['Ace', 'King', 'Queen', 'Jack',
               'Ten', 'Nine', 'Eight', 'Seven',
               'Six', 'Five', 'Four', 'Three', 'Two'];
+
+//DOM Variables
+let textArea = document.getElementById('text-area');
+let newGameButton = document.getElementById('new-game-button');
+let hitButton = document.getElementById('hit-button');
+let stayButton = document.getElementById('stay-button');
+
+//Game Variables
+let gameStarted = false,
+    gameOver = false,
+    playerWon = false,
+    dealerCards = [],
+    playerCards = [],
+    dealerScore = 0,
+    playerScore = 0,
+    deck = [];
+
+hitButton.style.display = 'none';
+stayButton.style.display = 'none';
+showStatus();
+
+newGameButton.addEventListener('click', function() {
+  gameStarted = true;
+  gameOver = false;
+  playerWon = false;
+
+  deck = createDeck();
+  dealerCards = [ getNextCard(), getNextCard() ];
+  playerCards = [ getNextCard(), getNextCard() ];
+
+  newGameButton.style.display = 'none';
+  hitButton.style.display = 'inline';
+  stayButton.style.display = 'inline';
+  showStatus();
+});
 
 function createDeck() {
   let deck = [];
@@ -25,6 +61,13 @@ function createDeck() {
 
 function getCardString(card) {
   return card.value + " of " + card.suit;
+}
+
+function showStatus() {
+  if (!gameStarted) {
+    textArea.innerText = "Care to play a game, buddy?";
+    return;
+  }
 }
 
 function getNextCard() {
